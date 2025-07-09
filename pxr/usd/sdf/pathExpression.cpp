0a5cc99dd6a8a887e35b31f8169737884b60bf9d
wthpix <wthpix@users.noreply.github.com>
2025-07-08 17:02:28 -0700

    fix clang warning: lambda capture 'opStack' is not used
    (Internal change: 2372296)

--- pxr/usd/sdf/pathExpression.cpp
+++ pxr/usd/sdf/pathExpression.cpp
@@ -398,9 +398,7 @@ SdfPathExpression::GetText() const
         return "<unknown>";
     };
 
-    std::vector<Op> opStack;
-
-    auto printLogic = [&opName, &opStack, &result](
+    auto printLogic = [&opName, &result](
         std::vector<std::pair<Op, int>> const &stack) {
 
         const Op op = stack.back().first;
