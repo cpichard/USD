5a64a000cfac4bdbb84387f91f8a3ab1b870fb8c
gitamohr <gitamohr@users.noreply.github.com>
2026-02-05 21:20:35 -0800

    usd: Provide an optimization analogous to one removed in 2393523 where when computing ResolveInfo, if we're doing so for the purposes of a UsdAttribute::Get(), we eagerly fetch default and fallback values instead of just their types.  This way we can avoid refetching them later during _GetValueFromResolveInfoImpl.
    (Internal change: 2394884)

--- pxr/usd/usd/stage.h
+++ pxr/usd/usd/stage.h
@@ -2022,28 +2022,17 @@ private:
     // Default & TimeSample Resolution
     // --------------------------------------------------------------------- //
 
-    void _GetResolveInfo(const UsdAttribute &attr, 
-                         UsdResolveInfo *resolveInfo,
-                         const UsdTimeCode *time = nullptr) const;
-
-    void _GetResolveInfoWithResolveTarget(
-        const UsdAttribute &attr, 
-        const UsdResolveTarget &resolveTarget,
-        UsdResolveInfo *resolveInfo,
-        const UsdTimeCode *time = nullptr) const;
-
-    template <class T=VtValue> struct _ExtraResolveInfo;
+    struct _ExtraResolveInfo;
 
     // Gets the value resolve info for the given attribute. If time is provided,
     // the resolve info is evaluated for that specific time (which may be 
     // default). Otherwise, if time is null, the resolve info is evaluated for
     // "any numeric time" and will not populate values in extraInfo that 
     // require a specific time to be evaluated.
-    template <class T>
     void _GetResolveInfo(const UsdAttribute &attr, 
                          UsdResolveInfo *resolveInfo,
                          const UsdTimeCode *time = nullptr,
-                         _ExtraResolveInfo<T> *extraInfo = nullptr) const;
+                         _ExtraResolveInfo *extraInfo = nullptr) const;
 
     // Gets the value resolve info for the given attribute using the given 
     // resolve target. If time is provided, the resolve info is evaluated for 
@@ -2051,29 +2040,28 @@ private:
     // the resolve info is evaluated for "any numeric time" and will not 
     // populate values in extraInfo that require a specific time to be 
     // evaluated.
-    template <class T>
     void _GetResolveInfoWithResolveTarget(
         const UsdAttribute &attr, 
         const UsdResolveTarget &resolveTarget,
         UsdResolveInfo *resolveInfo,
         const UsdTimeCode *time = nullptr,
-        _ExtraResolveInfo<T> *extraInfo = nullptr) const;
+        _ExtraResolveInfo *extraInfo = nullptr) const;
 
     // Shared implementation function for _GetResolveInfo and 
     // _GetResolveInfoWithResolveTarget. The only difference between how these
     // two functions behave is in how they create the Usd_Resolver used for 
     // iterating over nodes and layers, thus they provide this implementation
     // with the needed MakeUsdResolverFn to create the Usd_Resolver.
-    template <class T, class MakeUsdResolverFn>
+    template <class MakeUsdResolverFn>
     void _GetResolveInfoImpl(const UsdAttribute &attr, 
                          UsdResolveInfo *resolveInfo,
                          const UsdTimeCode *time,
-                         _ExtraResolveInfo<T> *extraInfo,
+                         _ExtraResolveInfo *extraInfo,
                          const MakeUsdResolverFn &makeUsdResolveFn) const;
 
     struct _BracketingSamplesResolver;
     struct _PropertyStackResolver;
-    template <class T> struct _ResolveInfoResolver;
+    struct _ResolveInfoResolver;
     struct _SamplesInIntervalResolver;
     struct _TimeSampleMapResolver;
 
@@ -2130,15 +2118,14 @@ private:
         UsdTimeCode time, const UsdAttribute &attr,
         Usd_Interpolator const &interpolator,
         const UsdResolveInfo &infoIn, const UsdResolveTarget *resolveTarget,
-        const _ExtraResolveInfo<T> *extraInfo, T *result) const;
+        const _ExtraResolveInfo *extraInfo, T *result) const;
 
-    template <class T>
     bool _GetCompletedResolveInfo(const UsdAttribute &attr,
                                   UsdTimeCode time,
                                   const UsdResolveTarget *resolveTarget,
                                   const UsdResolveInfo &infoIn,
                                   UsdResolveInfo *infoOut,
-                                  _ExtraResolveInfo<T> *extraInfoOut) const;
+                                  _ExtraResolveInfo *extraInfoOut) const;
 
     Usd_AssetPathContext
     _GetAssetPathContext(UsdTimeCode time, const UsdAttribute &attr) const;
